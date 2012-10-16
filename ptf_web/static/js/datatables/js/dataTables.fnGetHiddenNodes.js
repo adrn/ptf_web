4dced052414a20886e9510595de74815303a9e9e
Adrian Price-Whelan <adrian.prw@gmail.com>
2012-10-16 10:20:42 -0400

    add detailed coverage page

---
+++ ptf_web/static/js/datatables/js/dataTables.fnGetHiddenNodes.js
@@ -0,0 +1,30 @@
+$.fn.dataTableExt.oApi.fnGetHiddenNodes = function ( oSettings )
+{
+    /* Note the use of a DataTables 'private' function thought the 'oApi' object */
+    var anNodes = this.oApi._fnGetTrNodes( oSettings );
+    var anDisplay = $('tbody tr', oSettings.nTable);
+      
+    /* Remove nodes which are being displayed */
+    for ( var i=0 ; i<anDisplay.length ; i++ )
+    {
+        var iIndex = jQuery.inArray( anDisplay[i], anNodes );
+        if ( iIndex != -1 )
+        {
+            anNodes.splice( iIndex, 1 );
+        }
+    }
+      
+    /* Fire back the array to the caller */
+    return anNodes;
+};
+
+$.fn.dataTableExt.oApi.fnGetVisibleNodes = function ( oSettings )
+{
+    /* Note the use of a DataTables 'private' function thought the 'oApi' object */
+    var anNodes = this.oApi._fnGetTrNodes( oSettings );
+    //var anDisplay = $('tbody tr', oSettings.nTable);
+    var anDisplay = $('tbody tr');
+    
+    /* Fire back the array to the caller */
+    return anDisplay;
+};
